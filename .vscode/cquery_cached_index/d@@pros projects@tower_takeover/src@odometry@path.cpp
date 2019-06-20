1f61efb5c3e1223b6dbac17d1bfb10bd8c79c7ca
Geonaylor <7kvexrobot@gmail.com>
2019-06-19 21:14:45 -0700

    Display

---
+++ .vscode/cquery_cached_index/d@@pros projects@tower_takeover/src@odometry@path.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "okapi/api.hpp"
+using namespace okapi;
+
+namespace path {
+  void generate() {
+    //AsyncMotionProfileController::generatePath(Bruh, "Je suis le pamplemousse");
+  }
+}
