0003760acd534cf0eb29107cc11d156c7cf8372d
Latte <clearloadsave@gmail.com>
2019-07-24 19:38:33 -0700

    Ignore

--- .vscode/cquery_cached_index/e@@repo@pros@tower_takeover/src@config@macro.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-void wait(int ms) { // Just does pros::delay, so nothing to worry about
-  pros::delay(ms);
-}
-
-void print(const char * text) {
-  std::cout << text << std::endl;
-}
