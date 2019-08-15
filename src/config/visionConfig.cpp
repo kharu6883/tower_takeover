70c184160b37c7d272a2bcb4470cc073806eee7d
Latte <clearloadsave@gmail.com>
2019-08-15 00:18:41 -0700

    Comment edits

--- src/config/visionConfig.cpp
+++ src/config/visionConfig.cpp
@@ -2,8 +2,8 @@
 
 // Signatures
 pros::vision_object_s_t CUBE_PURPLE = {1, VISION_OBJECT_NORMAL, 3, 735, 1503, 1118, 5839, 7681, 6760 };
-pros::vision_object_s_t CUBE_GREEN = {2, VISION_OBJECT_NORMAL, 3, -6691, -5621, -6156, -3801, -2585, -3194 };
-pros::vision_object_s_t CUBE_ORANGE = {3, VISION_OBJECT_NORMAL, 3, 4831, 6585, 5708, -2495, -1933, -2214 };
+// pros::vision_object_s_t CUBE_GREEN = {2, VISION_OBJECT_NORMAL, 3, -6691, -5621, -6156, -3801, -2585, -3194 };
+// pros::vision_object_s_t CUBE_ORANGE = {3, VISION_OBJECT_NORMAL, 3, 4831, 6585, 5708, -2495, -1933, -2214 };
 pros::vision_object_s_t SIG_4 = {4, VISION_OBJECT_NORMAL, 0, 0, 0, 0, 0, 3, 0};
 pros::vision_object_s_t SIG_5 = {5, VISION_OBJECT_NORMAL, 0, 0, 0, 0, 0, 3, 0};
 pros::vision_object_s_t SIG_6 = {6, VISION_OBJECT_NORMAL, 0, 0, 0, 0, 0, 3, 0};
