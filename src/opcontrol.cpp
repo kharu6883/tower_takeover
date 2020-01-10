3101ead434c2e970a88939092216180977ebb0f6
7K Robotics <7kvexrobot@gmail.com>
2020-01-09 19:45:38 -0800

    Improved rack PID & function

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -16,8 +16,8 @@ void opcontrol() {
 	Rack rack;
 	Arm arm;
 
-	macro::Slew rackSlew(80, 80, true); // Accel, Decel
-	macro::PID rackPID(0.08); // kP
+	macro::Slew rackSlew(7, 7, true); // Accel, Decel
+	macro::PID rackPID(0.1); // kP
 	macro::Slew roller(5, 10); // Accel, Decel
 
 	rack.setBrakeType(MOTOR_BRAKE_HOLD);
@@ -34,44 +34,26 @@ void opcontrol() {
 		RF.move_velocity(-master.get_analog(ANALOG_LEFT_Y) * 1.58 + master.get_analog(ANALOG_LEFT_X) * 1.58 + master.get_analog(ANALOG_RIGHT_X) * 1.58);
 		RB.move_velocity(-master.get_analog(ANALOG_LEFT_Y) * 1.58 - master.get_analog(ANALOG_LEFT_X) * 1.58 + master.get_analog(ANALOG_RIGHT_X) * 1.58);
 
-		if(master.get_digital(DIGITAL_A)) {
-			if(!isTrack) isTrack = true;
-				else isTrack = false;
-			while(master.get_digital(DIGITAL_A)) pros::delay(20);
-		}
-
 		/*--------------------------------
 		    RACK
 		--------------------------------*/
 		if(master.get_digital(DIGITAL_L1) && !master.get_digital(DIGITAL_L2)) {
 
-			if(!isTrack) { // Put up Rack
-				lastPos = 2;
-
-				rackPID.withConst(0.08, 0).calculate(RACK_UP, *rack.getPot());
-				rackSlew.withLimit(rackPID.getOutput()).calculate(rackPID.getOutput());
-
-		  } else { // Tower Placement
-				lastPos = 1;
-
-				rackPID.withConst(0.4).calculate(RACK_TOWER, *rack.getPot());
-				rackSlew.withLimit(rackPID.getOutput()).calculate(rackPID.getOutput());
-
-			}
+			lastPos = 1;
+			rackPID.calculate(RACK_UP, *rack.getPot());
+			rackSlew.withLimit(rackPID.getOutput()).calculate(rackPID.getOutput());
 
 		} else if(master.get_digital(DIGITAL_L2) && !master.get_digital(DIGITAL_L1)) { // Goin' Down
-			lastPos = 0;
 
-			rackPID.withConst(0.1).calculate(RACK_DOWN, *rack.getPot());
+			lastPos = 0;
+			rackPID.calculate(RACK_DOWN, *rack.getPot());
 			rackSlew.withLimit(rackPID.getOutput()).calculate(rackPID.getOutput());
 
 		} else { // Stop
 
-			if(lastPos == 2) {
+			if(lastPos == 1) {
 				rackPID.calculate(RACK_UP, *rack.getPot());
-			} else if(lastPos == 1) {
-				rackPID.calculate(RACK_TOWER, *rack.getPot());
-			} else if(lastPos == 0) {
+			} else {
 				rackPID.calculate(RACK_DOWN, *rack.getPot());
 			}
 
